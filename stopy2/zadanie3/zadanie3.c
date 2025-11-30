aa65c1b6d84045b1477ad3c9cdbeac420374ad75
Julian <01187284@pw.edu.pl>
2025-11-30 21:05:15 +0000

    fast

---
+++ stopy2/zadanie3/zadanie3.c
@@ -0,0 +1,115 @@
+#define _GNU_SOURCE
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAX_N 100
+
+#define ERR(source) \
+    (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
+
+volatile sig_atomic_t work = 0, pause_work = 0, work2 = 0;
+pid_t children[MAX_N];
+
+void usr1_handler(int sig){
+    work = 1;
+    pause_work = 0;
+}
+
+void usr1_handler2(int sig){
+    work2 = 1;
+}
+
+void usr2_handler(int sig){
+    pause_work = 1;
+    work = 0;
+}
+
+void sethandler(void (*f)(int), int sigNo){
+    struct sigaction act;
+    memset(&act, 0, sizeof(struct sigaction));
+    act.sa_handler = f;
+    if(-1 == sigaction(sigNo, &act, NULL)){
+        ERR("sigaction");
+    }
+}
+
+void child_work(){
+    int count = 0;
+    printf("My pid:%d\n", getpid());
+    srand(getpid());
+    sethandler(usr1_handler, SIGUSR1);
+    sethandler(usr2_handler, SIGUSR2);
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+        while(!work){
+            sigsuspend(&oldmask);
+        }
+        while(1){
+            if(!pause_work){
+                int r = 100 + rand() % (200 - 100 + 1);
+            struct timespec t = {1, 0};
+            nanosleep(&t, NULL);
+            count++;
+            printf("%d: %d\n", getpid(), count);
+            }
+            else{
+                sigsuspend(&oldmask);
+            }
+        }
+}
+
+void create_children(int n){
+    pid_t s;
+    for(int i=0;i<n;i++){
+        if((s = fork()) < 0){
+            ERR("fork");
+        }
+        if(s == 0){
+            child_work();
+            exit(EXIT_SUCCESS);
+        }
+        else{
+            children[i] = s;
+        }
+    }
+}
+
+int main(int argc, char** argv){
+    printf("PARENT PID: %d\n", getpid());
+    if(argc != 2){
+        perror("zle wejscie");
+    }
+    int n = atoi(argv[1]);
+    create_children(n);
+    int i = 0;
+    sethandler(usr1_handler2, SIGUSR1);
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+    kill(children[0], SIGUSR1);
+    while(1){
+        while(!work2){
+            sigsuspend(&oldmask);
+        }
+        work2 = 0;
+        kill(children[i], SIGUSR2);
+        i = (i+1) % n;
+        kill(children[i], SIGUSR1);
+    }
+    while(wait(NULL) > 0){
+
+    }
+    return EXIT_SUCCESS;
+}
